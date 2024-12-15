a39984925a1f03e74c2af37921d74715b0dad9c8
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2024-12-15 17:32:18 +0900

    u

--- source/run.sh
+++ source/run.sh
@@ -4,13 +4,6 @@ fileName=$1
 
 # Compile
 # https://docs.google.com/spreadsheets/d/1HXyOXt5bKwhKWXruzUvfMFHQtBxfZQ0047W7VVObnXI/edit?gid=408033513#gid=408033513&range=M43
-# g++-12 -std=gnu++2b -Og -g -fsanitize=undefined,address \
-#     -DLOCAL \
-#     -I/home/penguin8331/vscode/cpp/library/dump \
-#     -I/opt/ac-library -I/opt/boost/gcc/include -L/opt/boost/gcc/lib \
-#     -o a.out ${fileName} \
-#     -lgmpxx -lgmp \
-#     -I/usr/include/eigen3
 g++-12 -std=gnu++2b -O2 -DONLINE_JUDGE -DATCODER \
     -Wall -Wextra \
     -mtune=native -march=native \
