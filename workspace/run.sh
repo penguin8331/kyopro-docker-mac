d7c6ea194e75cc6d191a1e9f0be3c5110636275c
penguin8331 <shota.qdmt3vba4ls@gmail.com>
2025-08-15 21:15:16 +0900

    u

--- workspace/run.sh
+++ workspace/run.sh
@@ -5,6 +5,7 @@ fileName=$1
 # Compile
 # https://docs.google.com/spreadsheets/d/1HXyOXt5bKwhKWXruzUvfMFHQtBxfZQ0047W7VVObnXI/edit?gid=408033513#gid=408033513&range=M43
 g++-12 -std=gnu++2b -O2 -DONLINE_JUDGE -DATCODER \
+    -DLOCAL
     -Wall -Wextra \
     -mtune=native -march=native \
     -fconstexpr-depth=2147483647 -fconstexpr-loop-limit=2147483647 -fconstexpr-ops-limit=2147483647 \
